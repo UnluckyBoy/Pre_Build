e012af431ef902f23b7a2b681b22d65ffbc98794
UnluckyBoy <qfmy_zhang@163.com>
2022-08-12 16:32:58 +0800

    By Matrix

--- Pre_Build_3d/Work_3d.py
+++ Pre_Build_3d/Work_3d.py
@@ -9,6 +9,17 @@ import argparse
 import random
 import pandas as pd
 
+def Get_Index_By_GetIndex_File(file_path):
+    with open(file_path,'r',encoding='utf-8') as read_file:
+        str_split_2_list=(read_file.read()).split('\n')#str_split_2_list是llist数据类型
+        # print(str_split_2_list)
+        for i in range(len(str_split_2_list)):
+            str_temp=str_split_2_list[i]
+            pass
+        print("最后一个:",str_temp)
+        pass
+    pass
+
 def DoForecast_CSV_File(file_path,column):
     """获取csv文件内容并返回
     :param file_path: 文件地址,类型为str
@@ -62,24 +73,32 @@ def GetMin(num_01,num_02,num_03):
     #return num_01 if num_01 > num_02 and num_01 > num_03 else num_02 if num_02 > num_03 else num_03#最大数
     pass
 
-def GetResult_Funcation_01(list_01,list_02,list_03):
-    """
-    #将list_01,list_02,list_03中的单元素组合成一个list元素
-    :param list_01:
-    :param list_02:
-    :param list_03:
-    :return: 返回组合完成后且去重的list
-    """
-    index = GetMin(len(list_01), len(list_02), len(list_03))
+def GetResult_Funcation_01(args):
+    """定义储存csv获取到三列数的数组"""
+    result_num_01 = DoForecast_CSV_File(args.csv_path, 0)
+    result_num_02 = DoForecast_CSV_File(args.csv_path, 1)
+    result_num_03 = DoForecast_CSV_File(args.csv_path, 2)
+    # print("获取结果:"+"\nresult_num_01:" + str(result_num_01)+"\nresult_num_02:"+str(result_num_02)+"\nresult_num_03:"+str(result_num_03))
+    ###此处为方法一使用数据###
+    indexList = [7, 5, 3]  # 手动输入上一期数字
+    result_index_01 = GetNext_Index(indexList[0], result_num_01)
+    result_index_02 = GetNext_Index(indexList[1], result_num_02)
+    result_index_03 = GetNext_Index(indexList[2], result_num_03)
+
+    index = GetMin(len(result_index_01), len(result_index_02), len(result_index_03))
 
     result=[]
     for i in range(index):
-        result.append(str(list_01[i])+ str(list_02[i]) + str(list_03[i]))
+        result.append(str(result_index_01[i])+ str(result_index_02[i]) + str(result_index_03[i]))
         pass
 
     result_Remove_duplicate = Get_Remove_duplicate(result)#去重
-    return result_Remove_duplicate
+
+    ###调用封装的显示方法显示结果###
+    Show_Result(indexList, result_Remove_duplicate)
     pass
+
+
 
 def Get_Remove_duplicate(list):
     """
@@ -96,7 +115,6 @@ def Get_Remove_duplicate(list):
     return result_Remove_duplicate
     pass
 
-
 def Show_Result(indexList,result_list):
     str_show = ''
     for i in range(len(result_list)):
@@ -108,31 +126,15 @@ def Show_Result(indexList,result_list):
     pass
 
 def main(args):
-    """定义储存csv获取到三列数的数组"""
-    result_num_01 =DoForecast_CSV_File(args.file_path,0)
-    result_num_02 =DoForecast_CSV_File(args.file_path,1)
-    result_num_03 =DoForecast_CSV_File(args.file_path,2)
-    #print("获取结果:"+"\nresult_num_01:" + str(result_num_01)+"\nresult_num_02:"+str(result_num_02)+"\nresult_num_03:"+str(result_num_03))
-
-    """
-    ###此处为方法一使用数据###
-    """
-    indexList=[7,5,3]#上一期数字
-    result_index_01=GetNext_Index(indexList[0],result_num_01)
-    result_index_02=GetNext_Index(indexList[1],result_num_02)
-    result_index_03=GetNext_Index(indexList[2],result_num_03)
-    #使用方法一预测
-    result_Funcation_01=GetResult_Funcation_01(result_index_01,result_index_02,result_index_03)
-    ###调用封装的显示方法显示结果###
-    Show_Result(indexList,result_Funcation_01)
-
+    #GetResult_Funcation_01(args)
+    Get_Index_By_GetIndex_File(args.next_path)
     pass
 
 if __name__=='__main__':
     #file_dir = r"./work.csv"
     parser = argparse.ArgumentParser()
     parser.add_argument('--csv_path', type=str,default='./data/work_3d.csv',help='csv文件地址')
-    parser.add_argument('--index_path', type=str, default='./data/work_3d_GetIndex.txt', help='csv文件地址')
-    parser.add_argument('--next_path', type=str, default='./data/work_3d_GetNext.txt', help='csv文件地址')
+    parser.add_argument('--next_path', type=str, default='./data/work_3d_GetIndex.txt', help='csv文件地址')
+    parser.add_argument('--index_path', type=str, default='./data/work_3d_Index.txt', help='csv文件地址')
     args = parser.parse_args()
     main(args)
