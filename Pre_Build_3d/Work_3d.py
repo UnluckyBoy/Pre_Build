e7b49a3bbc81631f833984d37c7e346e895d21eb
UnluckyBoy <qfmy_zhang@163.com>
2022-08-12 18:22:25 +0800

    By Matrix

--- Pre_Build_3d/Work_3d.py
+++ Pre_Build_3d/Work_3d.py
@@ -9,15 +9,64 @@ import argparse
 import random
 import pandas as pd
 
-def Get_Index_By_GetIndex_File(file_path):
+def Get_Index_By_Index_File(file_path):
+    """
+    #读取file_path文件获取上一期号码
+    :param file_path:
+    :return:
+    """
     with open(file_path,'r',encoding='utf-8') as read_file:
         str_split_2_list=(read_file.read()).split('\n')#str_split_2_list是llist数据类型
         # print(str_split_2_list)
+        result = []
+
+        #读取无符号“,”的文件操作,如:652
         for i in range(len(str_split_2_list)):
-            str_temp=str_split_2_list[i]
+            str_temp=str_split_2_list[i]#str_temp类型为str
             pass
-        print("最后一个:",str_temp)
+        # print(str_temp[0]+str_temp[1]+str_temp[2])#打印前三个字符
+        for j in range(len(str_temp)):
+            # print(str_temp[j])
+            result.append(int(str_temp[j]))
+            pass
+
+        # 读取有符号“,”的文件操作,如:6,5,2
+        # get_str_temp_element=str_temp.split()
+        # for j in range(len(get_str_temp_element)):
+        #     str_temp_int=int(get_str_temp_element[j])
+        #     result.append(str_temp_int)
+        #     pass
+        # #print("最后一个:",result)
+
+        return result
         pass
+    pass
+
+def Get_Next_By_Next_File(file_path):
+    with open(file_path,'r',encoding='utf-8') as read_file:
+        str_split_2_list = (read_file.read()).split('\n')  # str_split_2_list是llist数据类型
+        # print(str_split_2_list)
+        next_list=[]
+        for i in range(len(str_split_2_list)):
+            str_split_2_list_element_temp=[]
+            for j in range(3):
+                str_split_2_list_element_temp.append(int(str_split_2_list[i].split(',')[j]))#取前三个数
+                pass
+            # print(str_split_2_list_element_temp)#打印str_split_2_list_element_temp全部元素
+            next_list.append(str_split_2_list_element_temp)
+            pass
+        # print(str_split_2_list_element_temp)#打印str_split_2_list_element_temp最后一个元素
+        print(next_list)
+        pass
+    pass
+
+def Get_Index_Sum(index_list):
+    """
+    #求list元素和
+    :param index_list:
+    :return:
+    """
+    return index_list[0]+index_list[1]+index_list[2]
     pass
 
 def DoForecast_CSV_File(file_path,column):
@@ -73,17 +122,17 @@ def GetMin(num_01,num_02,num_03):
     #return num_01 if num_01 > num_02 and num_01 > num_03 else num_02 if num_02 > num_03 else num_03#最大数
     pass
 
-def GetResult_Funcation_01(args):
+def GetResult_Funcation_01(args,index_list):
     """定义储存csv获取到三列数的数组"""
     result_num_01 = DoForecast_CSV_File(args.csv_path, 0)
     result_num_02 = DoForecast_CSV_File(args.csv_path, 1)
     result_num_03 = DoForecast_CSV_File(args.csv_path, 2)
-    # print("获取结果:"+"\nresult_num_01:" + str(result_num_01)+"\nresult_num_02:"+str(result_num_02)+"\nresult_num_03:"+str(result_num_03))
+
     ###此处为方法一使用数据###
-    indexList = [7, 5, 3]  # 手动输入上一期数字
-    result_index_01 = GetNext_Index(indexList[0], result_num_01)
-    result_index_02 = GetNext_Index(indexList[1], result_num_02)
-    result_index_03 = GetNext_Index(indexList[2], result_num_03)
+    # index_list = [7, 5, 3]  # 手动输入上一期数字
+    result_index_01 = GetNext_Index(index_list[0], result_num_01)
+    result_index_02 = GetNext_Index(index_list[1], result_num_02)
+    result_index_03 = GetNext_Index(index_list[2], result_num_03)
 
     index = GetMin(len(result_index_01), len(result_index_02), len(result_index_03))
 
@@ -95,9 +144,8 @@ def GetResult_Funcation_01(args):
     result_Remove_duplicate = Get_Remove_duplicate(result)#去重
 
     ###调用封装的显示方法显示结果###
-    Show_Result(indexList, result_Remove_duplicate)
+    Show_Result_Save(index_list, result_Remove_duplicate,args.result_path)
     pass
-
 
 
 def Get_Remove_duplicate(list):
@@ -115,26 +163,54 @@ def Get_Remove_duplicate(list):
     return result_Remove_duplicate
     pass
 
-def Show_Result(indexList,result_list):
+def Show_Result_Save(index_list,result_list,save_path):
     str_show = ''
     for i in range(len(result_list)):
         str_show += str(result_list[i]) + " "
         pass
-    print(str_show)
-    print("数量:", len(result_list))
-    print("上一期:" + str(indexList))
+    # print(str_show)
+    # print("数量:", len(result_list))
+    # print("上一期:" + str(index_list))
+    count_str="\n数量:"+str(len(result_list))
+    index_list_str="\t上一期:" + str(index_list)
+    result_save_str=str_show+count_str+index_list_str
+
+    with open(save_path,'w+',encoding='utf-8') as write_file:
+        write_file.write(result_save_str)
+        pass
+    print("保存完毕！***"+format(save_path))
+    pass
+
+def Save_CSV_File(file_path,index_list):
+    """
+    #保存文件
+    :param file_path:
+    :param index_list:
+    :return:
+    """
+    sum_2_str=str(Get_Index_Sum(index_list))
+    index_list_split_element_2_str=str(index_list[0])+","+str(index_list[1])+","+str(index_list[2])
+    index_list_2_str=str(index_list[0])+str(index_list[1])+str(index_list[2])
+    str_save=index_list_split_element_2_str+","+index_list_2_str+","+sum_2_str
+    with open(file_path,'a+',encoding='utf-8') as write_file:
+        write_file.write(str_save+"\n")
+        pass
+    print("保存完毕！"+format(file_path))
     pass
 
 def main(args):
-    #GetResult_Funcation_01(args)
-    Get_Index_By_GetIndex_File(args.next_path)
+    index_list=Get_Index_By_Index_File(args.index_path)
+    print("上一期:",index_list)
+    # Get_Next_By_Next_File(args.next_path)
+    GetResult_Funcation_01(args,index_list)#预测下一期结果,在data/work_3d_result.txt处查看
+    Save_CSV_File(args.csv_path,index_list)#保存(即更新csv结果池)
     pass
 
 if __name__=='__main__':
-    #file_dir = r"./work.csv"
     parser = argparse.ArgumentParser()
     parser.add_argument('--csv_path', type=str,default='./data/work_3d.csv',help='csv文件地址')
-    parser.add_argument('--next_path', type=str, default='./data/work_3d_GetIndex.txt', help='csv文件地址')
-    parser.add_argument('--index_path', type=str, default='./data/work_3d_Index.txt', help='csv文件地址')
+    parser.add_argument('--next_path', type=str, default='./data/work_3d_GetNext.txt', help='work_3d_GetNext.txt文件地址')
+    parser.add_argument('--index_path', type=str, default='./data/work_3d_Index.txt', help='work_3d_Index.txt文件地址')
+    parser.add_argument('--result_path', type=str, default='./data/work_3d_result.txt', help='结果work_3d_result.txt文件保存地址')
     args = parser.parse_args()
     main(args)
